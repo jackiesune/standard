ea75fd4917bdbf531469acebf6bc9b638982d48b
jack.s <my_email@exp.co>
2018-12-04 17:40:22 +0800

    add cookie

--- main.py
+++ main.py
@@ -1,16 +1,60 @@
 #main.py
-
-import urllib.request
-import urllib.parse
-import urllib.error
-
-
-#urlopen(url,data,timeout...)
-#Request(url,data,headers,method...)
+import http.cookiejar
+from urllib.request import HTTPCookieProcessor,build_opener
+#import urllib.request
 url="http://python.org"
-headers={
-    "User-Agent":""
-}
-req=urllib.request.Request(url=url,headers=headers)
-response=urllib.request.urlopen(req)
-print(response.status)
+def not_file():
+    cookie=http.cookiejar.CookieJar()
+    handler=HTTPCookieProcessor(cookie)
+    opener=build_opener(handler)
+    response=opener.open(url)
+    for item in cookie:
+        print(item.name+':'+item.value)
+
+
+def mozilla_cookie():
+    filename='cookieMozilla'
+    cookie=http.cookiejar.MozillaCookieJar(filename)
+    handler=HTTPCookieProcessor(cookie)
+    opener=build_open(handler)
+    response=opener.open(url)
+    cookie.save(ignore_discard=True,ignore_expirs=True)
+
+
+def LWP_cookie():
+    filename='cookieLWP'
+    cookie=http.cookiejar.LWPCookieJar(filename)
+    handler=HTTPCookieProcessor(cookie)
+    opener=build_open(handler)
+    response=opener.open(url)
+    cookie.save(ignore_discard=True,ignore_expirs=True)
+
+
+
+def load_LWP_cookie():
+    filename='cookieLWP'
+    cookie=http.cookiejar.CookieJar()
+    cookie.load(filename,ignore_discard=True,ignore_expires=True)
+    handler=HTTPCookieProcessor(cookie)
+    opener=build_open(handler)
+    response=opener.open(url)
+    print(response.read().decode('utf8'))
+
+
+
+          
+          
+          
+def main():
+
+
+    order=input("which would you want to run:not_file(n),mozilla(m),LWP_cookie(l),load(o)")
+    if order=='n':not_file()
+    if order=='m':mozilla_cookie()
+    if order=='l':LWP_cookie()
+    if order=='o':load_LWP_cookie()
+        
+
+
+main()
+
