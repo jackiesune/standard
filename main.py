48b68cd233639f2925fb3392723692ed7de24091
jack.s <my_email@exp.co>
2018-12-04 10:54:40 +0800

    add url

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+#main.py
+
 import urllib.request
 import urllib.parse
 import urllib.error
 
 
+#urlopen(url,data,timeout...)
+#Request(url,data,headers,method...)
+url="http://python.org"
+headers={
+    "User-Agent":""
+}
+req=urllib.request.Request(url=url,headers=headers)
+response=urllib.request.urlopen(req)
+print(response.status)
