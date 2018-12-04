d135922b363eec687e0abf763c16820e2a55a23c
jack.s <my_email@exp.co>
2018-12-04 11:50:24 +0800

    test

--- main.py
+++ main.py
@@ -7,10 +7,25 @@ import urllib.error
 
 #urlopen(url,data,timeout...)
 #Request(url,data,headers,method...)
-url="http://python.org"
+#url="http://python.org"
+url="https://movie.douban.com/"
 headers={
-    "User-Agent":""
+    "User-Agent":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0",
+    "Host":"movie.douban.com"
 }
 req=urllib.request.Request(url=url,headers=headers)
 response=urllib.request.urlopen(req)
 print(response.status)
+html=response.read().decode('utf8')
+print(html)
+choose='''<body>.*?<.*?wrapper.*?>.*?content.*?>.*?<.*?list-wp.*?>.*?<.*?slider-wrapper.*?>.*?slide-page.*?data-index="(/d)">.*?<p>(.*?).*?<strong>(.*?)</strong>'''
+pattern=re.compile(choose)
+results=re.findall(pattern,html)
+
+
+
+
+
+
+
+
